a20354f85f83f12dc2f9274628aa2b752e1add5e
Richa Basak <richa-basak@users.noreply.github.com>
2024-10-29 19:02:37 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript for smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
